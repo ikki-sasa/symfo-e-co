e2266d3097045ce756aa2dd5c54c032f73384956
ikki-sasa <manga.samadjine@3wa.io>
2022-03-02 10:18:40 +0100

    starting project

---
+++ src/Controller/TestController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestController
+{
+    public function index()
+    {
+        dd('ça fonctionne bien meme');
+    }
+
+
+
+    /**
+     * @Route("/test/{age<\d+>?0}", name="test", methods={"GET", "POST"}, host="localhost", schemes={"http, "https"})
+     */
+    public function test(Request $request, $age)
+    {
+        // $request = Request::createFromGlobals();
+
+        // dump($request);
+
+        // $age = $request->query->get('age', 0);
+        // $age = $request->attributes->get('age');
+
+        return new Response("Vous avez $age ans !");
+    }
+}
