92c04e824b3c62c20f4d4a7f52a44cfb35585835
ikki-sasa <manga.samadjine@3wa.io>
2022-03-04 15:26:05 +0100

    mise en place de doctrine et de entity

--- readme.md
+++ readme.md
@@ -39,4 +39,29 @@ Question8
 ````
 Question9
 ````
+## Repository and Controller 
 ````
+// count([])
+//find(id)
+// findBy([], [])
+//findOneBy([], [])
+// findAll()
+````
+
+## Doctrine 
+```
+ORM : Object relationnel Mapping librairie correspondance les objest et la bdd orm fait tout par objet pas de contact direct avec la bdd
+
+ENTITY: qui représente ligne de nos tables
+
+MANAGER/CONTROLLER: permet de manipuler les lignes d'objets qui sera transferer vers les tabs dans les fichiers Controller
+
+REPOSITORY: permet de faire des selections remonter les enregistrement de la bdd directement au seins du projet sous forme d'objet
+
+Migrations: fichier représente la bdd pour bosser en équipe 
+```
+
+## Fixtures 
+remplir une bdd avec de fausses informations et de fausses données sans avoir a taper à la main 
+composer require string
+doctirne:fixtures:load
--- src/Controller/HelloWorldController.php
+++ src/Controller/HelloWorldController.php
@@ -2,30 +2,34 @@
 
 namespace App\Controller;
 
-use App\Taxes\Calculator;
-use Cocur\Slugify\Slugify;
-use Psr\Log\LoggerInterface;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HelloWorldController extends AbstractController
 {
+
     /**
      * @Route("/hello/{name?}", name="hello")
      */
     // public function hello($name)
-    public function hello($name = "world", LoggerInterface $logger, Calculator $calculator, Slugify $slugify)
+    public function hello($prenom = "world")
     {
-        // Environment $twig dump($twig);
+        // $html = $this->twig->render('hello.html.twig', [
+        //     'prenom' => $prenom
+        // ]);
+        // return new Response($html);
+        return $this->render('hello.html.twig', [
+            'prenom' => $prenom
+        ]);
+    }
 
-        dump($slugify->slugify("hello World"));
-
-        $logger->error("mon message log");
-
-        $tva = $calculator->calcul(100);
-        dump($tva);
-
-        return new Response("Hello $name");
+    /**
+     * @Route("/example", name="example")
+     */
+    public function example()
+    {
+        return $this->render('example.html.twig', [
+            'age' => 33
+        ]);
     }
 }
